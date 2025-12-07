ec4ca606ca290c8919de97bfad2e77c81b245851
winston <wwzhen1083728594@gmail.com>
2025-12-08 07:52:59 +0800

    release 1.2.0

--- miniprogram/envList.js
+++ miniprogram/envList.js
@@ -6,7 +6,7 @@ const envList = {
   // 🛠️ 开发/测试环境 (Development)
   // 用于开发调试，数据可以随便造
   dev: {
-    envId: "cloud1-0g4462vv9d9954a5", // 目前你正在用的这个作为开发环境
+    envId: "", // 目前你正在用的这个作为开发环境
     name: "开发环境",
   },
 
